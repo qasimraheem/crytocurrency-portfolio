59e902da71b9ea31afaa2ecca93cf6ca574b37c5
Qasim Raheem <qasimmehmood13936@gmail.com>
2018-01-17 21:53:03 +0500

    Update catchacrypto.cpp

--- catchacrypto.cpp
+++ catchacrypto.cpp
@@ -283,13 +283,14 @@ long double  double_validation()
 	cout << "iquantity=" << iquantity;
 	return iquantity;
 }
-
+//color function
 void text(int text_color = 7 on int paper_color = 0)
 { 
 	// defaults to light_gray on black
 	int color_total = (text_color + (paper_color * 16));
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color_total);
 }
+
 void in_dash()
 {
 	ifstream in_dash;
